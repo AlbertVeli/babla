213b859f4faab0ba4231050101949f5d0d91072c
Albert Veli <albert.veli@gmail.com>
2012-12-06 19:57:40 +0100

    Fix path so script can be called from other dirs.

--- svensk-engelsk.sh
+++ svensk-engelsk.sh
@@ -13,9 +13,10 @@ bail_out()
 }
 
 srcword=${1}
+scriptdir=`dirname $0`
 
 if test "${srcword}" = ""; then
-    for i in `dirname $0`/*.sh; do
+    for i in ${scriptdir}/*.sh; do
     basename $i .sh
     done
     bail_out '^^ choose one of the from-to combinations above'
@@ -23,4 +24,5 @@ fi
 
 fromto=`basename $0 .sh`
 
-html2text --ignore-emphasis --ignore-links --ignore-images "http://sv.bab.la/lexikon/${fromto}/${srcword}" 2>/dev/null | ./svensk-engelsk-helper.pl "${srcword}"
+/usr/bin/html2text --ignore-emphasis --ignore-links --ignore-images "http://sv.bab.la/lexikon/${fromto}/${srcword}" 2>/dev/null | \
+${scriptdir}/svensk-engelsk-helper.pl "${srcword}"
